ec11bf40ba0d906865b9c2d7452b5534e0e266f6
Butrint Komoni <butrintkomonih@gmail.com>
2020-03-13 13:30:24 +0100

    Update o365-enum.py

--- o365-enum.py
+++ o365-enum.py
@@ -11,17 +11,18 @@ from email_split import email_split
 class o365:
 
     def identify_o365(email_domain):
-        email_domain = email_split(open(args.list).readline().rstrip())
 
+        email_domain = email_split(open(args.list).readline().rstrip())
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
             'Sec-Fetch-Dest': 'document'
         }
-
         url_structure = "https://login.microsoftonline.com/{}/.well-known/openid-configuration".format(email_domain.domain)
         r = requests.get(url_structure, headers=headers)
         subscribed = re.search('token_endpoint', r.text)
 
+        
+        
         # AADSTS90002	InvalidTenantName - The tenant name wasn't found in the data store. Check to make sure you have the correct tenant ID.
         if '"error_codes":[90002]' in r.text: #If not subscribed
             print("The domain isn't using o365")
@@ -32,8 +33,9 @@ class o365:
 
 
     def enum_company(users):
-        users = open(args.list, "r")
 
+        users = open(args.list, "r")
+        
         while True:
             line = users.readline()
             if not line:
@@ -41,13 +43,13 @@ class o365:
 
             url_schema = "https://login.microsoftonline.com/common/GetCredentialType"
             r = requests.post(url_schema, json={"Username": line.strip()})
-#            print(r.text)
 
             response = r.text
             exists =  re.search('"IfExistsResult":0,', response)
             nonexistent = re.search('"IfExistsResult":1,', response)
 
             init()
+
 
             if exists:
                 print(colored(line.strip(), 'blue'))
@@ -56,8 +58,7 @@ class o365:
                 print(colored(line.strip(), 'red'))
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter, description='O365 enum')
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description='o365 enum')
     parser.add_argument('-l', '--list', required=True, type=str, help='username list')
 
     args = parser.parse_args()
